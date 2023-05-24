5b501411397ee9892eb12c3d75d2107e317a2f2b
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2023-05-24 17:43:48 +0200

    AAAAH (#5)

--- src/router/Routeur.js
+++ src/router/Routeur.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "../pages/Home";
 import Layout from '../components/layout/Layout';
+import Error404 from '../error/Error404';
 
 const Routeur = () => {
     return ( 
@@ -9,6 +10,7 @@ const Routeur = () => {
             <Layout>
                 <Routes>
                     <Route path='/' element={<Home/>}/>
+                    <Route path='*' element={<Error404/>}/>
                 </Routes>
             </Layout>
         </Router>
