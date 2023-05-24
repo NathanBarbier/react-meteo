46ee9a2431ab0e98e58173b5fa50690723464910
Lansor <killian28100@gmail.com>
2023-05-24 21:40:43 +0200

    fichier pas passé

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import SearchForm from '../components/SearchForm'
 import axios from 'axios'
+import WeatherDisplay from '../components/WeatherDisplay'
 
 const Home = () => {
-    const [data, setData] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
     const handleSearch = (searchTerm) => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&lang=fr&units=metric&appid=6193f9c91e0d8975adb8c302aeaf5862`
-        fetchData(url)
+        setSearchTerm(searchTerm)
     }
-
-    const fetchData = async (url) => {
-        try {
-            const response = await axios.get(url)
-            setData(response.data)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    
-    console.log(data)
 
     return (
         <>
@@ -31,7 +19,13 @@ const Home = () => {
                 <SearchForm onSearch={handleSearch} />
             </div>
 
-            <div>{data.name}</div>
+            <div>
+                { searchTerm  ? (
+                    <WeatherDisplay searchTerm={searchTerm} />
+                )
+            : null}
+                
+            </div>
         </>
     )
 }
